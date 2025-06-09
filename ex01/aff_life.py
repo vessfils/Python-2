35d2fe62ff37efc244b0d987f221e00aadad153a
vessfils <vesslan@hotmail.fr>
2025-06-09 07:14:30 +0200

    ex01 finito

--- ex01/aff_life.py
+++ ex01/aff_life.py
@@ -6,17 +6,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def display_graph():
-    data = load("life_expectancy_yfafaears.csv")
-    #on va convertir la columns en int
-    data.columns = data.columns.astype(int)
-    X = data.columns 
-    Y = data.loc["France"]
-    plt.plot(X, Y, color='blue', label='Life expectancy line')
-    plt.xlabel('Year')
-    plt.ylabel('Life expectancy')
-    plt.title('France Life expectancy Projections')
-    plt.legend()
-    plt.show()
+    try:
+        data = load("life_expectancy_years.csv")
+        if data is None:
+            return
+        #on va convertir la columns en int
+        data.columns = data.columns.astype(int)
+        country = "France"
+        if country not in data.index:
+            raise ValueError("The country is not found in the dataset.")
+        X = data.columns 
+        Y = data.loc[country]
+        plt.plot(X, Y, color='blue', label='Life expectancy line')
+        plt.xlabel('Year')
+        plt.ylabel('Life expectancy')
+        plt.title(f'{country} - Life expectancy Projections')
+        plt.legend()
+        plt.show()
+    except Exception as e:
+        print(f"❌ Value error: {e}")
 
 def main():
    display_graph()
